c6d57788c1494d20254f8ca356dedc9de1a075fe
deepu nair <dpnr915@gmail.com>
2023-03-12 23:54:45 +0530

    fixing bugs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ function Card(props) {
           <div className="card-body">
             <h5 className="card-title">{props.name.length>60? props.name.substr(0,40)+"..." : props.name}</h5>
             <p className="card-text">{props.name.length>60? props.name.substr(0,40)+"..." : props.name}</p>
-            <Link to={resolvd.pathname.includes('/products/category')?`/products/${props.id}`:`/products/category/${props.name}`} className="btn btn-primary " onClick={()=>props.onClick(props.id)}>Go somewhere</Link>
+            <Link to={resolvd.pathname.includes('/products/category')||resolvd.pathname.includes('/products')?`/products/${props.id}`:`/products/category/${props.name}`} className="btn btn-primary " onClick={()=>props.onClick(props.id)}>Go somewhere</Link>
           </div>
         </div>
     );
